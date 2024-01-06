cf514bfe5ca285dd9268966e621cd06982c98eb9
Recitasse <raphael.gougeonpro@gmail.com>
2024-01-07 00:10:51 +0100

    upd

--- config/Variables/variables.py
+++ config/Variables/variables.py
@@ -1,12 +1,12 @@
 # CONFIG
-GLOBAL_PATH = "/home/raphael/Bureau/Programmation/Python/Onche beta/"
+GLOBAL_PATH = "/home/raphael/Bureau/Programmation/Python/Onche/"
 
 # Mysql
 SAVE_FREQUENCY = 10
 MYSQL_USER = 'onche'
 MYSQL_PASSWORD = 'OnchePass1#'
 MYSQL_DATABASE = 'Onche'
-MYSQL_HOST = 'localhost'
+MYSQL_HOST = ''
 MYSQL_PATH_EXPORT = GLOBAL_PATH + "BDD/exports/"
 
 # Encryption
@@ -21,9 +21,6 @@ PATH_WEB_BROWSER = GLOBAL_PATH + "webscrapper/browser.log"
 FORUM_XML = GLOBAL_PATH + "config/forums.xml"
 
 # Browser
-BINARY = "/usr/bin/firefox"
-PROFILE = GLOBAL_PATH + "utils/browser/profile"
-GECKO = GLOBAL_PATH + "utils/browser/geckodriver.exe"
 BASE_URL = "https://onche.org/forum/"
 USER_AGENT = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 8_6_7; en-US) Gecko/20100101 Firefox/70.4"
 DEFAULT_PROFILE = GLOBAL_PATH + "utils/cryptage/profile/default.json"
--- install.sh
+++ install.sh
@@ -73,9 +73,6 @@ PATH_WEB_BROWSER = GLOBAL_PATH + "webscrapper/browser.log"
 FORUM_XML = GLOBAL_PATH + "config/forums.xml"
 
 # Browser
-BINARY = "/usr/bin/firefox"
-PROFILE = GLOBAL_PATH + "utils/browser/profile"
-GECKO = GLOBAL_PATH + "utils/browser/geckodriver.exe"
 BASE_URL = "https://onche.org/forum/"
 USER_AGENT = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 8_6_7; en-US) Gecko/20100101 Firefox/70.4"
 DEFAULT_PROFILE = GLOBAL_PATH + "utils/cryptage/profile/default.json"
